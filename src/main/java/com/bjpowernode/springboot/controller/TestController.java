44c48d35c55830455ad28daf16c40489b99ddab0
cm <cm@dalian.com>
2019-10-18 22:29:00 +0800

    陈明  修改第二次

--- src/main/java/com/bjpowernode/springboot/controller/TestController.java
+++ src/main/java/com/bjpowernode/springboot/controller/TestController.java
@@ -1,18 +1,1 @@
-package com.bjpowernode.springboot.controller;
-
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.web.bind.annotation.*;
-
-
-public class TestController {
-
-    @RequestMapping("/test/{name}/{age}")
-
-    public Object test(@PathVariable("name") String name,
-                       @PathVariable("age") String age){
-        System.out.println(123);
-        return "name ="+name +"age="+age;
-        System.out.println(111111111111111111111111111111);
-        1233156415645;
-    }
-}
+dfsgdfsgdssdfggdsfgsd
